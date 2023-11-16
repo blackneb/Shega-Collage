34cd5e8c7d15e670c8babcd15121b48f54a19722
blackneb <antenehcs@gmail.com>
2023-11-17 00:38:01 +0300

    modeling

--- frontend/src/components/Students.tsx
+++ frontend/src/components/Students.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@mui/material"
+import AddIcon from '@mui/icons-material/Add';
 const Students = () => {
   return (
-    <div className="max-h-[calc(100vh-80px)]">
-      
+    <div className="max-h-[calc(100vh-80px)] w-full">
+      <div>
+        <Button variant="contained" startIcon={AddIcon} style={{ backgroundColor: '#4CAF50', marginTop:'5px' }}>Add New Student</Button>
+      </div>
     </div>
   )
 }
